5fbfd9050fd063ee34159a4d7e91fa7b3c4ad718
Fredrik <fredrik.jonson@student.kyh.se>
2022-01-13 13:53:14 +0100

    Add project files.

---
+++ FictionalCustomer.WebApp/Data/SeedAdminAccount.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FictionalCustomer.WebApp.Data
+{
+    public class SeedAdminAccount
+    {
+        public static void Initizialize(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+                //context.Database.EnsureCreated();
+                //context.Database.Migrate();
+                CreateAdminUser(userManager, roleManager, configuration);
+            }
+        }
+
+        private static void CreateAdminUser(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            var role = "Admin";
+            var adminUserName = configuration?["AdminInfo:ADMINUSER"];
+            var adminPassword = configuration?["AdminInfo:ADMINPASSWORD"];
+            if (string.IsNullOrWhiteSpace(adminUserName) || string.IsNullOrWhiteSpace(adminPassword))
+            {
+                throw new ArgumentNullException(adminUserName, adminPassword);
+            }
+
+            bool foundRole = roleManager.RoleExistsAsync(role).Result;
+
+            if (!foundRole)
+            {
+                var adminRole = roleManager.CreateAsync(new IdentityRole(role)).GetAwaiter().GetResult();
+            }
+
+            var adminUser = userManager.FindByNameAsync(adminUserName).Result;
+            if (adminUser is null)
+            {
+                adminUser = new IdentityUser
+                {
+                    UserName = adminUserName,
+                    Email = adminUserName,
+                };
+                var created = userManager.CreateAsync(adminUser, adminPassword).Result;
+
+                if (created.Succeeded)
+                {
+                    var confirmationToken = userManager.GenerateEmailConfirmationTokenAsync(adminUser).Result;
+                    _ = userManager.ConfirmEmailAsync(adminUser, confirmationToken).Result;
+                    _ = userManager.AddToRoleAsync(adminUser, role).Result;
+                }
+            }
+        }
+    }
+}
---
+++ FictionalCustomer.WebApp/Program.cs
@@ -0,0 +1,54 @@
+using FictionalCustomer.WebApp.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+//
+SeedAdminAccount.Initizialize(app.Services);
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/Identity/Account/Register", context => Task.Factory.StartNew(() => context.Response.Redirect("/Identity/Account/Login", true, true)));
+    endpoints.MapPost("/Identity/Account/Register", context => Task.Factory.StartNew(() => context.Response.Redirect("/Identity/Account/Login", true, true)));
+});
+app.MapRazorPages();
+
+app.Run();
